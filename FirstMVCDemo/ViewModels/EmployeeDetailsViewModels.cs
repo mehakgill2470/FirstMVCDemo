4aade8e8594fd741b48d54642c04c0ca1b002c88
Mehak Gill <mehakgill2470@gmail.com>
2020-04-02 21:08:24 -0400

    first commit

---
+++ FirstMVCDemo/ViewModels/EmployeeDetailsViewModels.cs
@@ -0,0 +1,13 @@
+﻿
+
+using FirstMVCDemo.Models;
+namespace FirstMVCDemo.ViewModels
+{
+    public class EmployeeDetailsViewModel
+    {
+        public Employee Employee { get; set; }
+        public Address Address { get; set; }
+        public string PageTitle { get; set; }
+        public string PageHeader { get; set; }
+    }
+}
